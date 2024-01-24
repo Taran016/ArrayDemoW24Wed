2f12b570885c9c9642e33c684daa5c4b5e5a03f8
Taranpreet Singh <sin14121@sheridancollege.ca>
2024-01-24 10:21:16 -0500

    added part time student class

---
+++ src/helloworld/newclass.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helloworld;
+
+/**
+ *
+ * @author Taranpreet Singh
+ */
+public class newclass extends student{
+    private int numOfCourses;
+
+    
+    public newclass(int numOfCourses, int sId, String sname) {
+        super(sId, sname);
+        this.numOfCourses = numOfCourses;
+    }
+    public int getNumOfCourses(){
+        
+    }
+    
+    
+}
